8696b4482b31bb9ae4ef9ed6f774230e12b196c4
Christian Bayan Francia <pipoyfrancia0322@gmail.com>
2026-05-21 20:24:28 +0800

    add: deletePost

--- backend/src/controllers/blogController.js
+++ backend/src/controllers/blogController.js
@@ -32,3 +32,15 @@ export async function updatePost(req, res) {
         res.status(500).json({message: "Server Error"});
     }
 }
+
+export async function deletePost(req, res) {
+    try{
+        let {title, content} = req.body;
+        let deletedPost = await Post.findByIdAndDelete(req.params.id, {title, content}, {new: true});
+        if(!deletedPost) return res.status(404).json({message: "Post not found"});
+        res.status(200).json({message: "Post deleted successfully"});
+    }catch (error) {
+        res.status(500).json({message: "Server Error"});
+    }
+
+}
--- backend/src/routes/postRoute.js
+++ backend/src/routes/postRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {getAllPosts, createPost, updatePost} from '../controllers/blogController.js';
+import {getAllPosts, createPost, updatePost, deletePost} from '../controllers/blogController.js';
 
 let router = express.Router();
 
 router.get("/", getAllPosts);
 router.post("/", createPost);
 router.put("/:id", updatePost);
+router.delete("/:id", deletePost);
 
 export default router

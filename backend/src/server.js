97d0f7d5f4e31a328a5b544673a0ab7dbfdce7d4
Christian Bayan Francia <pipoyfrancia0322@gmail.com>
2026-05-21 18:54:44 +0800

    connected to db

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import {connectDB} from './config/db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+let app = express();
+
+
+app.get("/api/blog", (req, res) => {
+    res.send("Hello World");
+});
+
+
+connectDB();
+app.listen(5003, () => {
+    console.log("Server is running on port 5003");
+}) 
